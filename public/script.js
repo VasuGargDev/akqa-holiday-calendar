f95de4c554235494bd1c1b281b9c8834fc9cc0b4
aryan-deep <69759033+aryan-deep@users.noreply.github.com>
2022-08-30 17:25:14 +0530

    updated js

--- public/script.js
+++ public/script.js
@@ -1,32 +1,28 @@
-// creating a model
+
 let modal = document.getElementById("myModal");
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
-const monthhead=document.querySelector(".date h1");
-const datehead=document.querySelector(".date p");
-const modellabel= document.querySelector(".model-label")
-const monthDays = document.querySelector(".days");
-const mon=document.getElementById("Months")
-const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+let modalcontent=document.querySelector("#modal-content")
+let span = document.getElementsByClassName("close")[0];
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+const dates=["2022-01-14","2022-01-26","2022-03-01","2022-03-18","2022-04-15","2022-05-03","2022-08-11","2022-08-15","2022-08-19","2022-10-05","2022-10-24","2022-10-25","2022-10-26","2022-11-08"]
 
 
-// Current date and time from browser
 const date = new Date();
-mon.value=date.getMonth()
-
-mon.addEventListener('change',()=>{
-  let option=mon.options[mon.selectedIndex]
-  
-  date.setMonth(option.value)
-  Calendar();
-})
-const currentmonth=()=>{
-  let option=parseInt(mon.selectedIndex);
-  return option+1;
-}
-
-// fetching the json data from holiday.json
+console.log(date)
 const jsondata=()=>{
+
   fetch('holiday.json')
     .then(response=>response.json())
     .then(data=>{
@@ -34,37 +30,34 @@ const jsondata=()=>{
         for(let l=0;l<data.length;l++){
           let holidayMonth=parseInt(data[l].date.slice(5,7));
           let currentMonth= currentmonth(); 
-          if(data[l].date && holidayMonth===currentMonth){
+          if(dates.includes(data[l].date) && holidayMonth===currentMonth){
               let holidayDate= parseInt(data[l].date.slice(-2)); 
               let element=document.getElementById(`${holidayDate}`);
               if(data[l].type==="floating"){
                     element.classList.add("floating");
                     element.addEventListener("click",()=>{
                       modal.style.display = "block";
-                      modellabel.innerHTML=data[l].label
+                      modalcontent.classList.add("floating")
+                      document.querySelector("#model-label").innerHTML=data[l].label
                     })
                   }
               else if(data[l].type==="public"){
                     element.classList.add("public");
                     element.addEventListener("click",()=>{
                       modal.style.display = "block";
-                      modellabel.innerHTML=data[l].label
+                      modalcontent.classList.add("public")
+                      document.querySelector("#model-label").innerHTML=data[l].label
                     })
                   }
                 }
+            }
+          })
         }
-    })
-}
 
+const renderCalendar = () => {
+  date.setDate(1);
+  const monthDays = document.querySelector(".days");
 
-const Calendar = () => {
-
-
-
-
-// Getting the last day of the current month like for march it is 31
-// 0 in the argument gives the last day
-// getDate gives the only date 
   const lastDay = new Date(
     date.getFullYear(),
     date.getMonth() + 1,
@@ -72,40 +65,37 @@ const Calendar = () => {
   ).getDate();
 
  
-// date.getDay gives the index of the week day like 1 for monday
+
   const firstDayIndex = date.getDay();
-  const lastDayIndex=new Date(date.getFullYear(),
-  date.getMonth()+1,0).getDay();
-  const nextDays=7-lastDayIndex-1;
- 
+  console.log("firstDayIndex="+firstDayIndex)
 
-// date.getMonth() gives the index of the current month like 0 for January
-monthhead.innerHTML = months[date.getMonth()];
+  const lastDayIndex = new Date(
+    date.getFullYear(),date.getMonth() + 1, 0 ).getDay();
 
-// date.toDateString gives the current day date year
-datehead.innerHTML = new Date().toDateString();
+  const nextDays = 7 - lastDayIndex - 1;
+
+
+  document.querySelector(".date h1").innerHTML = months[date.getMonth()];
+
+  document.querySelector(".date p").innerHTML = new Date().toDateString();
 
   let days = "";
-// creating empty divs as first day may not be sunday
+
   for (let x = firstDayIndex; x > 0; x--) {
     days += `<div class="prev-date">${""}</div>`;
   }
 
-// Looping through the first day to last day of the month
-// checking the date is todays date
   for (let i = 1; i <= lastDay; i++) {
 
-    if (
-      i === new Date().getDate() &&
-      date.getMonth() === new Date().getMonth()
-    ) {
-      days += `<div class="today" id="${i}">${i}</div>`;
+    if (i === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
+      days += `<div class="today day" id="${i}">${i}</div>`;
     } else {
-      days += `<div id="${i}">${i}</div>`;
+      days += `<div class="day" id="${i}">${i}</div>`;
     }
   }
-  for(let j=1;j<nextDays;j++){
-    days+=`<div class="next-date>${""}</div>`
+
+  for (let j = 1; j <=nextDays; j++) {
+    days += `<div class="next-date">${""}</div>`;
   }
 
 
@@ -120,26 +110,37 @@ jsondata()
 
 
 
+const mon=document.getElementById("Months")
+mon.addEventListener('change',()=>{
+    let option=mon.options[mon.selectedIndex]
+    // console.log(months[option.value])
+    date.setMonth(option.value)
+    renderCalendar();
+})
 
+const currentmonth=()=>{
+    let option=parseInt(mon.selectedIndex);
+    return option+1;
+}
+mon.value=date.getMonth()
 
-
-
+function darkMode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
 
 span.onclick = function() {
     modal.style.display = "none";
+    modalcontent.removeAttribute("class")
   }
   
-  // When the user clicks anywhere outside of the modal, close it
+
   window.onclick = function(event) {
     if (event.target == modal) {
       modal.style.display = "none";
+      modalcontent.removeAttribute("class")
     }
-  }
-
-  function darkMode() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
   }
   
 
-Calendar();
+renderCalendar();
